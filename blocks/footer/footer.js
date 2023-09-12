157d1b9b94823b18d497a5222908fd69593d0f9e
rachel.lohmeyer <rachel.lohmeyer@netcentric.biz>
2023-09-12 23:14:01 +0200

    cleanup

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -18,6 +18,27 @@ export default async function decorate(block) {
     // decorate footer DOM
     const footer = document.createElement('div');
     footer.innerHTML = html;
+    const externalLinks = footer.querySelectorAll(':scope > div:nth-child(2)');
+    const socialLinks = footer.querySelectorAll(':scope > div:nth-child(3)');
+    const logoContainer = footer.querySelectorAll(':scope > div:nth-child(4)');
+
+    externalLinks.forEach((linkList) => {
+      linkList.classList.add('external-links');
+    });
+
+    socialLinks.forEach((linkList) => {
+      linkList.classList.add('social-links');
+    });
+
+    logoContainer.forEach((container) => {
+      container.classList.add('logo-container');
+    });
+
+    footer.querySelectorAll('a > span.icon').forEach((icon) => {
+      const a = icon.parentElement;
+      const iconName = icon.classList[1].substring(5, icon.classList[1].length);
+      a.title = a.title || iconName;
+    });
 
     // add link title for all icon only links
     footer.querySelectorAll('a > span.icon').forEach((icon) => {
